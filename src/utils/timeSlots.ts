0e97251aedf529812de2b2c176ca814dd6416c4c
Matteo Bacci <matteo.bacci@nearform.com>
2025-05-09 10:19:57 +0200

    fixed timeslots

--- src/utils/timeSlots.ts
+++ src/utils/timeSlots.ts
@@ -7,23 +7,23 @@ type TimeSlotDetails = {
 
 export const timeSlots: Record<TimeSlot, TimeSlotDetails> = {
   1: {
-    label: "Sabato 9.00 - 12.00",
+    label: "Sabato 10.00 - 13.00",
     className: "timeslot-pink-500",
   },
   2: {
-    label: "Sabato 12.00 - 16.00",
+    label: "Sabato 15 - 19.00",
     className: "timeslot-fuchsia-500",
   },
   3: {
-    label: "Sabato 16.00 - 20.00",
+    label: "Sabato 21.30 - 00.30",
     className: "timeslot-rose-500",
   },
   4: {
-    label: "Domenica 10.00 - 14.00",
+    label: "Domenica 10.00 - 13.00",
     className: "timeslot-indigo-500",
   },
   5: {
-    label: "Domenica 14.00 - 18.00",
+    label: "Domenica 15.00 - 19.00",
     className: "timeslot-teal-500",
   },
 } as const satisfies Record<TimeSlot, TimeSlotDetails>;
